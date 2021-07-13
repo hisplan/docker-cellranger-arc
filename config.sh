6f4a2b13dc2ad11ff300fa7e326884e6e084f140
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2021-07-13 11:44:08 -0400

    Upgrade to Cell Ranger ARC v2

--- config.sh
+++ config.sh
@@ -1,4 +1,5 @@
-version="1.0.1"
+version="2.0.0"
+download_url="https://cf.10xgenomics.com/releases/cell-arc/cellranger-arc-2.0.0.tar.gz?Expires=1626223313&Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly9jZi4xMHhnZW5vbWljcy5jb20vcmVsZWFzZXMvY2VsbC1hcmMvY2VsbHJhbmdlci1hcmMtMi4wLjAudGFyLmd6IiwiQ29uZGl0aW9uIjp7IkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNjI2MjIzMzEzfX19XX0_&Signature=JllDxs-1BEeFugF-8LzNdDJj39qanqcwIlgtLqxxLTwsLi3jT6AJgvNgnt~vxltHBaoixe1CBXlQ6Ora2IexTYvBCHGPdIC-EtBNZ65PZK~yuxv7mJJMgjSfwl~6wlOCMKsjU3SwgilhaaEObuzhwk8ci44ZFy3km5RlDsJDg8EJ7GD7u3BHT-70mpPtqvBQyO-GS3XnM0xvaIKqV2NyLy~fh1EREzGn6lRfSVS3TKZOi9FYfQ8jFLq3VLfmNLL2euBNmzBukPiKwWUWeceMQpI9n8Rd31-7krJZCIwSzi8XnpMzAptUUNfR~s4rpFJaGfq-MKR2Nm9uvCawVhdlvQ__&Key-Pair-Id=APKAI7S6A5RYOXBWRPDA"
 
 # docker related
 registry="583643567512.dkr.ecr.us-east-1.amazonaws.com"
